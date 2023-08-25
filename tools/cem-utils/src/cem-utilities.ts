2169ec6be099c9a86023382410431dfa2ad9f944
Burton Smith <burton@breakstuff.io>
2023-08-25 09:24:29 -0400

    document functions

--- tools/cem-utils/src/cem-utilities.ts
+++ tools/cem-utils/src/cem-utilities.ts
@@ -4,10 +4,16 @@ import type { CEM, Component } from "./types";
 
 const EXCLUDED_TYPES = ["string", "boolean", "undefined", "number", "null"];
 
-export function getDescription(
+/**
+ * Gets the description from a CEM based on a specified source
+ * @param component CEM component/declaration object
+ * @param descriptionSrc property name of the description source
+ * @returns string
+ */
+export function getComponentDescription(
   component: schema.Declaration,
   descriptionSrc?: string
-) {
+): string {
   return (
     (descriptionSrc
       ? (component as any)[descriptionSrc]
@@ -16,6 +22,12 @@ export function getDescription(
   );
 }
 
+/**
+ * Gets a list of components from a CEM object
+ * @param customElementsManifest CEM object
+ * @param exclude and array of component names to exclude
+ * @returns Component[]
+ */
 export function getComponents(
   customElementsManifest: CEM,
   exclude?: string[]
@@ -31,8 +43,31 @@ export function getComponents(
     ) || []
   ).flat() as Component[];
 }
+/**
+ * Gets a list of public properties from a CEM component
+ * @param component CEM component/declaration object
+ * @returns an array of public properties for a given component
+ */
+export function getComponentProperties(component: Component) {
+  return component.members?.filter(
+    (member) =>
+      member.kind === "field" &&
+      member.privacy !== "private" &&
+      member.privacy !== "protected" &&
+      !(member as schema.CustomElementField).attribute
+  ) as schema.ClassMember[];
+}
 
-export function getAttributeValueOptions(attr: schema.Attribute, typeSrc: string = 'type'): string[] {
+/**
+ * This is intended for providing tooling a list of viable options for an attribute (excluding Js primitives).
+ * @param attr CEm attribute object
+ * @param typeSrc property name of the type source
+ * @returns list of values for a given attribute
+ */
+export function getAttributeValueOptions(
+  attr: schema.Attribute,
+  typeSrc: string = "type"
+): string[] {
   const value: string = (attr as any)[`${typeSrc}`]?.text || attr.type?.text;
   return !value
     ? []
@@ -41,7 +76,12 @@ export function getAttributeValueOptions(attr: schema.Attribute, typeSrc: string
         .map((type) => removeQuoteWrappers(type));
 }
 
-export function getMethods(component: Component) {
+/**
+ * Get all public methods for a component
+ * @param component CEM component/declaration object
+ * @returns ClassMethod[]
+ */
+export function getComponentMethods(component: Component): schema.ClassMethod[] {
   return component.members?.filter(
     (member) =>
       member.kind === "method" &&
