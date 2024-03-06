0bef4b384846ef35d49145857ffc7c203abd10fb
Nolan White <nolan.e.white@gmail.com>
2024-03-06 14:03:54 -0500

    Improve how JS properties are generated for Web Types
    Stop using attributes to generate JS properties
    Allow only public and non-static CEM class fields to be converted JS properties
    Fix JS properties not using correct format for type

--- packages/jet-brains-integration/src/types.d.ts
+++ packages/jet-brains-integration/src/types.d.ts
@@ -41,12 +41,19 @@ export interface WebTypeAttribute {
   value: WebTypeValue;
 }
 
+export interface WebTypeJsProperty {
+  name: string;
+  description?: string;
+  type?: string;
+  default?: string;
+}
+
 export interface WebTypeValue {
   type?: string | string[];
 }
 
 export interface JsProperties {
-  properties: WebTypeAttribute[];
+  properties: WebTypeJsProperty[];
   events: WebTypeEvent[];
 }
 
