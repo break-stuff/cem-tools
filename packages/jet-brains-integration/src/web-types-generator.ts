182347f97a2ef3f23b4a95d46889c34eb68c231e
Nolan White <nolan.e.white@gmail.com>
2024-03-08 12:41:17 -0500

    Revert removing deprecated "events" property from generated Web Types
    Older versions of JetBrains IDEs may still rely on the old schema, so leave this here for now

--- packages/jet-brains-integration/src/web-types-generator.ts
+++ packages/jet-brains-integration/src/web-types-generator.ts
@@ -56,6 +56,7 @@ export function getTagList(
           description: slot.description,
         };
       }),
+      events: getWebTypeEvents(component),
       js: getJsProperties(component, options.typesSrc),
     };
   });
