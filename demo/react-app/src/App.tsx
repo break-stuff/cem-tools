9d1e60401903eca37355cb5cbbb41d58e3e4714b
Burton Smith <burton@breakstuff.io>
2024-05-15 09:03:17 -0400

    add nextjs demo

--- demo/react-app/src/App.tsx
+++ demo/react-app/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <ScopeProvider prefix="test_" suffix="_test2">
+      <ScopeProvider>
         <RadioGroup>
           <RadioButton value=""></RadioButton>
           <RadioButton></RadioButton>
