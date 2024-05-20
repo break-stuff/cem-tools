40563ef4876ad8eaf616cacc47c1357d24994ef8
Burton Smith <burton@breakstuff.io>
2024-05-20 09:44:42 -0400

    fixed issue with "path" import

--- packages/custom-jsdoc-tags/src/cem-plugin.ts
+++ packages/custom-jsdoc-tags/src/cem-plugin.ts
@@ -1,6 +1,6 @@
 import { parse } from "comment-parser";
 import type { Component } from "../../../tools/cem-utils";
-import { logBlue, logYellow } from "../../../tools/integrations";
+import { logBlue, logYellow } from "../../../tools/integrations/src/logs";
 
 export interface Options {
   tags?: CustomTag;
