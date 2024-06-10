f432d813f3024763802a73b8b23e1c52c4ea9c24
break-stuff <burton@breakstuff.io>
2024-06-09 21:55:02 -0400

    update config to have optional parameters

--- packages/eslint-rules/src/types.ts
+++ packages/eslint-rules/src/types.ts
@@ -17,56 +17,56 @@ export type Options = {
   /** Adds a suffix to tag names */
   suffix?: string;
   /** Sets default rule error levels */
-  defaultRuleErrorLevels: {
+  defaultRuleErrorLevels?: {
     // custom-element eslint rules
     /** validates attribute where predefined values have been defined - default 'error' */
-    constrainedAttrs: ErrorLevel;
+    constrainedAttrs?: ErrorLevel;
     /** reports issue when boolean attributes have been assigned a value - default 'error' */
-    noBooleanAttrValues: ErrorLevel;
+    noBooleanAttrValues?: ErrorLevel;
     /** identifies attributes that have been deprecated - default 'warn' */
-    noDeprecatedAttrs: ErrorLevel;
+    noDeprecatedAttrs?: ErrorLevel;
     /** identifies elements that have been deprecated - default 'warn' */
-    noDeprecatedTags: ErrorLevel;
+    noDeprecatedTags?: ErrorLevel;
     /** identifies elements that require specific attributes - default 'error' */
-    requiredAttrs: ErrorLevel;
+    requiredAttrs?: ErrorLevel;
 
     // HTML eslint rules - https://html-eslint.org/docs/rules (these have all been disabled by default)
-    noDuplicateAttrs: ErrorLevel;
-    noDuplicateId: ErrorLevel;
-    noInlineStyles: ErrorLevel;
-    noObsoleteTags: ErrorLevel;
-    noRestrictedAttrValues: ErrorLevel;
-    noRestrictedAttrs: ErrorLevel;
-    noScriptStyleType: ErrorLevel;
-    noTargetBlank: ErrorLevel;
-    requireAttrs: ErrorLevel;
-    requireButtonType: ErrorLevel;
-    requireClosingTags: ErrorLevel;
-    requireDoctype: ErrorLevel;
-    requireLiContainer: ErrorLevel;
-    requireMetaCharset: ErrorLevel;
-    noMultipleH1: ErrorLevel;
-    requireLang: ErrorLevel;
-    requireMetaDescription: ErrorLevel;
-    requireOpenGraphProtocol: ErrorLevel;
-    requireTitle: ErrorLevel;
-    noAbstractRoles: ErrorLevel;
-    noAccesskeyAttrs: ErrorLevel;
-    noAriaHiddenBody: ErrorLevel;
-    noNonScalableViewport: ErrorLevel;
-    noPositiveTabindex: ErrorLevel;
-    noSkipHeadingLevels: ErrorLevel;
-    requireFrameTitle: ErrorLevel;
-    requireImgAlt: ErrorLevel;
-    requireMetaViewport: ErrorLevel;
-    elementNewline: ErrorLevel;
-    idNamingConvention: ErrorLevel;
-    indent: ErrorLevel;
-    lowercase: ErrorLevel;
-    noExtraSpacingAttrs: ErrorLevel;
-    noMultipleEmptyLines: ErrorLevel;
-    noTrailingSpaces: ErrorLevel;
-    quotes: ErrorLevel;
-    sortAttrs: ErrorLevel;
+    noDuplicateAttrs?: ErrorLevel;
+    noDuplicateId?: ErrorLevel;
+    noInlineStyles?: ErrorLevel;
+    noObsoleteTags?: ErrorLevel;
+    noRestrictedAttrValues?: ErrorLevel;
+    noRestrictedAttrs?: ErrorLevel;
+    noScriptStyleType?: ErrorLevel;
+    noTargetBlank?: ErrorLevel;
+    requireAttrs?: ErrorLevel;
+    requireButtonType?: ErrorLevel;
+    requireClosingTags?: ErrorLevel;
+    requireDoctype?: ErrorLevel;
+    requireLiContainer?: ErrorLevel;
+    requireMetaCharset?: ErrorLevel;
+    noMultipleH1?: ErrorLevel;
+    requireLang?: ErrorLevel;
+    requireMetaDescription?: ErrorLevel;
+    requireOpenGraphProtocol?: ErrorLevel;
+    requireTitle?: ErrorLevel;
+    noAbstractRoles?: ErrorLevel;
+    noAccesskeyAttrs?: ErrorLevel;
+    noAriaHiddenBody?: ErrorLevel;
+    noNonScalableViewport?: ErrorLevel;
+    noPositiveTabindex?: ErrorLevel;
+    noSkipHeadingLevels?: ErrorLevel;
+    requireFrameTitle?: ErrorLevel;
+    requireImgAlt?: ErrorLevel;
+    requireMetaViewport?: ErrorLevel;
+    elementNewline?: ErrorLevel;
+    idNamingConvention?: ErrorLevel;
+    indent?: ErrorLevel;
+    lowercase?: ErrorLevel;
+    noExtraSpacingAttrs?: ErrorLevel;
+    noMultipleEmptyLines?: ErrorLevel;
+    noTrailingSpaces?: ErrorLevel;
+    quotes?: ErrorLevel;
+    sortAttrs?: ErrorLevel;
   };
 };
